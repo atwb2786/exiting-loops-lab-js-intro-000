5f0b7ece292d46a11ddd7aac90bf99bbd9cf7f72
punctual-sailor-2214 <bchett286@yahoo.com>
2019-03-17 17:38:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,10 +26,7 @@ function findBy(array, findFn) {
   for (var i = 0; i < array.length; i++) {
     if(findFn(array[i])) {
       return array[i];
-    }else {
-      return null;
     }
-
   }
-
+  return null;
 }
